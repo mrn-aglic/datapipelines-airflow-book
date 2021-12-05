c4ce2c08a189d295d2fbf596040f3afab179f105
Marin Aglic <marin.aglic@gmail.com>
2021-12-05 21:09:31 +0100

    Added csv_to_json operator

---
+++ chapter09/tests/airflowbook/operators/test_json_to_csv_operator.py
@@ -0,0 +1,32 @@
+import csv
+import json
+from pathlib import Path
+
+from airflowbook.operators import JsonToCsvOperator
+
+
+def test_json_to_csv_operator(tmp_path: Path):
+    input_path = tmp_path / "input.json"
+    output_path = tmp_path / "output.csv"
+
+    # Write input data to tmp path
+    input_data = [
+        {"name": "bob", "age": "41", "sex": "M"},
+        {"name": "alice", "age": "24", "sex": "F"},
+        {"name": "carol", "age": "60", "sex": "F"},
+    ]
+
+    with open(input_path, "w") as f:
+        json.dump(input_data, f)
+
+    operator = JsonToCsvOperator(
+        task_id="test", input_path=input_path, output_path=output_path
+    )
+
+    operator.execute(context={})
+
+    with open(output_path, "r") as f:
+        reader = csv.DictReader(f)
+        result = [dict(row) for row in reader]
+
+    assert result == input_data
