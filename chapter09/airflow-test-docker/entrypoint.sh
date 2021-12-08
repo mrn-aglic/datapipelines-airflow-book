5c3d9fb5f4492a338e4b846101ca7c0b25f3106b
Marin Aglic <marin.aglic@gmail.com>
2021-12-08 22:16:43 +0100

    Added test_movielens_download_operator with dag

--- chapter09/airflow-test-docker/entrypoint.sh
+++ chapter09/airflow-test-docker/entrypoint.sh
@@ -4,6 +4,11 @@ cd /
 
 echo "Hello from test docker container!"
 
+# need db metastore for running tasks with .run method - airflow queries the metastore
+echo "initializing airflow db metastore..."
+airflow db init &> metastore.log
+echo "airflow metastore initialized!"
+
 echo "Listing dags:"
 ls -al dags/
 
@@ -13,6 +18,10 @@ ls -al tests/
 pytest tests/
 
 echo "Pytest exit code $?"
+
+# instead of using dbbeaver, let's print the metastore log rows
+echo "Metastore logs:"
+sqlite3 ////root/airflow/airflow.db "select * from log;" ".exit"
 
 # ignore errors 1 and 5 from pytest (1 - test failed, 5 - no tests found)
 exit $(( $? == 5 || $? == 1 ? 0 : $? ))
