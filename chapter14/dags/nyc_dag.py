89f0c4febf19113b87a39d35eb96b96f39168047
Marin Aglic <marin.aglic@gmail.com>
2022-02-23 18:51:15 +0100

    Implement taxi transform data operator

--- chapter14/dags/nyc_dag.py
+++ chapter14/dags/nyc_dag.py
@@ -1,5 +1,6 @@
 import json
 
+import pandas as pd
 import requests
 from airflow import DAG
 from airflow.hooks.base import BaseHook
@@ -7,7 +8,37 @@ from airflow.models import Variable
 from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.utils.dates import days_ago
-from requests.auth import HTTPBasicAuth
+from flask_httpauth import HTTPBasicAuth
+from nyctransport.operators.pandas_operator import PandasOperator
+from shared.minio_helpers import get_minio_object, write_minio_object
+
+
+def _download_taxi_data():
+    taxi_conn = BaseHook.get_connection(conn_id="taxi")
+
+    s3_hook = S3Hook(aws_conn_id="s3")
+
+    url = f"http://{taxi_conn.host}"
+    response = requests.get(url)
+    files = response.json()
+
+    exported_files = []
+
+    for filename in [f["name"] for f in files]:
+        response = requests.get(f"{url}/{filename}")
+        s3_key = f"raw/taxi/{filename}"
+        try:
+            s3_hook.load_string(
+                string_data=response.text,
+                key=s3_key,
+                bucket_name=Variable.get("MC_BUCKET_NAME"),
+            )
+            print(f"Uplaoded file {s3_key} to Minio.")
+            exported_files.append(s3_key)
+        except ValueError:
+            print(f"File {s3_key} already exists.")
+
+    return exported_files
 
 
 def _download_citibike_data(ts_nodash, **_):
@@ -21,12 +52,16 @@ def _download_citibike_data(ts_nodash, **_):
     data = response.json()
 
     s3_hook = S3Hook(aws_conn_id="s3")
-
-    s3_hook.load_string(
-        string_data=json.dumps(data),
-        key=f"raw/citibike/{ts_nodash}.json",
-        bucket_name=Variable.get("MC_BUCKET_NAME"),
-    )
+    s3_key = f"raw/citibike/{ts_nodash}.json"
+    try:
+        s3_hook.load_string(
+            string_data=json.dumps(data),
+            key=s3_key,
+            bucket_name=Variable.get("MC_BUCKET_NAME"),
+        )
+        print(f"Uploaded file {s3_key}")
+    except ValueError:
+        print(f"File {s3_key} already exists.")
 
 
 dag = DAG(
@@ -36,6 +71,30 @@ dag = DAG(
     catchup=False,
 )
 
+download_taxi_data = PythonOperator(
+    task_id="download_taxi_data", python_callable=_download_taxi_data, dag=dag
+)
+
+transform_taxi_data = PandasOperator(
+    task_id="process_taxi_data",
+    input_callable=get_minio_object,
+    input_callable_kwargs={
+        "pandas_read_callable": pd.read_csv,
+        "bucket": Variable.get("MC_BUCKET_NAME"),  # os.environ["MC_BUCKET_NAME"],
+        "paths": "{{ ti.xcom_pull(task_ids='taxi_extract_transform.download_taxi_data') }}",
+    },
+    transform_callable=None,
+    output_callable=write_minio_object,
+    output_callable_kwargs={
+        "bucket": Variable.get("MC_BUCKET_NAME"),  # os.environ["MC_BUCKET_NAME"],
+        "path": "processed/taxi/{{ ts_nodash }}.parquet",
+        "pandas_write_callable": pd.DataFrame.to_parquet,
+        "pandas_write_callable_kwargs": {"engine": "auto"},
+    },
+)
+
 download_citibike_data = PythonOperator(
     task_id="download_citibike_data", python_callable=_download_citibike_data, dag=dag
 )
+download_taxi_data >> transform_taxi_data
+download_citibike_data
